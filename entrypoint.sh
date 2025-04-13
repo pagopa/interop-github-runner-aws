049367a6908c7065eaebb7bc12152ac53687f423
Michele De Simone <michele.desimone@pagopa.it>
2025-04-13 22:03:10 +0200

    fix: env check

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,9 +8,9 @@ if [ "$(echo $INTERACTIVE_MODE | tr '[:upper:]' '[:lower:]')" == "true" ]; then
 fi
 
 # Verify some Repo URL and token have been given, otherwise we must be interactive mode.
-if [ -z "$GITHUB_REPOSITORY_URL" ] || [ -z "$GITHUB_TOKEN" ] || [ -z "$GITHUB_REPOSITORY_NAME" ]]; then
+if [ -z "$GITHUB_REPOSITORY_URL" ] || [ -z "$GITHUB_PAT" ] || [ -z "$GITHUB_REPOSITORY_NAME" ]]; then
 	if [ "$INTERACTIVE" == "FALSE" ]; then
-		echo "GITHUB_REPOSITORY_URL, GITHUB_TOKEN and GITHUB_REPOSITORY_NAME cannot be empty"
+		echo "GITHUB_REPOSITORY_URL, GITHUB_PAT and GITHUB_REPOSITORY_NAME cannot be empty"
 		exit 1
 	fi
 fi
